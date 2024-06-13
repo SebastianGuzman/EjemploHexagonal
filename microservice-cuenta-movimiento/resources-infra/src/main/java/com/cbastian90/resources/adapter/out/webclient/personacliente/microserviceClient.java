bfaaa4adb60ea8467d68c03434c826993ff50f71
cbastian900 <sebastian.guzman@udea.edu.co>
2024-06-12 20:34:07 -0500

    se agrega puerto base para consumo de microservicio cliente-persona

--- microservice-cuenta-movimiento/resources-infra/src/main/java/com/cbastian90/resources/adapter/out/webclient/personacliente/microserviceClient.java
+++ microservice-cuenta-movimiento/resources-infra/src/main/java/com/cbastian90/resources/adapter/out/webclient/personacliente/microserviceClient.java
@@ -1,8 +1,11 @@
 package com.cbastian90.resources.adapter.out.webclient.personacliente;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 @Slf4j
 @AllArgsConstructor
@@ -10,4 +13,35 @@ public class microserviceClient {
 
     private final WebClient webClient;
     private final String URI_MICRO_SERVICE = "/clientes";
+
+    public Mono<String> getPersonaCliente(Long id) {
+        log.info("Calling microservice");
+        return webClient.get()
+                .uri(URI_MICRO_SERVICE + "/" + id)
+                .exchange()
+                .flatMap(clientResponse -> {
+                    HttpStatus httpStatus = clientResponse.statusCode();
+
+                    if (httpStatus.is2xxSuccessful()) {
+                        log.info("getAccounts response {}", clientResponse);
+                        //TODO: Validar el código
+                        try {
+                            log.info("{}", new ObjectMapper().writeValueAsString(clientResponse));
+                        } catch (Exception e) {
+                            log.error("error al convertir el json:{}", e.getMessage());
+                        }
+
+                        return clientResponse.
+                                bodyToMono(String.class);
+                    } else {
+                        log.info("getAccounts clientResponse {}", clientResponse);
+                        return Mono.error(new RuntimeException("Error, Client Information details"));
+                    }
+                })
+                .doOnSuccess(m -> log.info("Video Base Log, Success: "))
+                .doOnError(error -> log.error(error.getMessage()))
+                .onErrorResume(Mono::error);
+
+    }
+
 }
--- microservice-cuenta-movimiento/resources-infra/src/main/java/com/cbastian90/resources/config/webclients/microserviceClientConfig.java
+++ microservice-cuenta-movimiento/resources-infra/src/main/java/com/cbastian90/resources/config/webclients/microserviceClientConfig.java
@@ -1,4 +1,20 @@
 package com.cbastian90.resources.config.webclients;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
 public class microserviceClientConfig {
+
+    @Value("${clients.persona-cliente.host}")
+    private String webClientCustomer;
+
+    @Bean
+    public WebClient getPersonaCliente(){
+        return WebClient.create(webClientCustomer);
+    }
+
+
 }
